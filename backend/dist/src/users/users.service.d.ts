a49bb7c1a00f79f7fc87541cc84fba7596c6c36e
unknown <you@example.com>
2026-09-02 17:17:17 +0100

    Nettoyage de Git et application du gitignore

--- backend/dist/src/users/users.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { User, Prisma } from '@prisma/client';
-export declare class UsersService {
-    findByEmail(email: string): Promise<User | null>;
-    findById(id: string): Promise<User | null>;
-    create(data: Prisma.UserCreateInput): Promise<User>;
-}
