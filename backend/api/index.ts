3a64897952540e88c8b887ee6cabc7e3f7893d13
unknown <you@example.com>
2026-09-02 21:34:41 +0100

    Configuration Vercel

---
+++ backend/api/index.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+
+const expressApp = express();
+
+let cachedApp;
+
+async function bootstrap() {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    app.enableCors({ origin: '*', credentials: true });
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    await app.init();
+    cachedApp = app;
+  }
+}
+
+export default async function (req, res) {
+  await bootstrap();
+  expressApp(req, res);
+}
